a40e202ed4c4dedb76ed96c5d899213ca11a2c47
Dinesh <1442dinesh6singh5@gmail.com>
2026-04-26 09:59:33 +0530

    refactor: modify auth store

--- src/libs/stores/useAuthStore.ts
+++ src/libs/stores/useAuthStore.ts
@@ -7,17 +7,15 @@ interface AuthState {
   logout: () => void
 }
 
-const rawData = JSON.parse(localStorage.getItem('auth-storage') || 'false')
-
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
-      isLoggedIn: rawData === true,
+      isLoggedIn: false, // Default is always false (safe for server)
       login: () => set({ isLoggedIn: true }),
       logout: () => set({ isLoggedIn: false }),
     }),
     {
-      name: 'auth-storage',
+      name: 'auth-storage', // The key in localStorage
     },
   ),
 )
